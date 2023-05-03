6a80cdaa62947922333708b716f9f913e508721e
siamCSEjnu <99203101+siamCSEJnU@users.noreply.github.com>
2023-05-03 22:28:38 +0600

    response for specific request specified

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express();
 const cors = require("cors");
 port = process.env.PORT || 5000;
 
+const chefs = require("./data/chefs.json");
+
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("hello");
+});
+
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
 });
 app.listen(port, () => {
   console.log(`the app is listening on port${port}`);
